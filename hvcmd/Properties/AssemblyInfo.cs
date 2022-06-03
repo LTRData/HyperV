651f915ba9d97eb6e101075226765ba19a991839
Olof Lagerkvist <ol@ltr-data.se>
2022-06-03 22:44:02 +0200

    Namespace and attributes adjustments

--- hvcmd/Properties/AssemblyInfo.cs
+++ hvcmd/Properties/AssemblyInfo.cs
@@ -5,14 +5,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HyperV")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Olof Lagerkvist")]
-[assembly: AssemblyProduct("HyperV")]
-[assembly: AssemblyCopyright("Copyright © Olof Lagerkvist 2012-2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,7 +24,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.*")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-
-[assembly: InternalsVisibleTo("testapp")]
